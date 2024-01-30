e9366f03eba51db41343ec5bcbc2b2a1571d9b79
guebebraga <guebe.braga5@gmail.com>
2024-01-30 18:00:14 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+//const routerIndex = require('./routes/index')
+
+const app = express()
+app.use(express.json())
+
+//app.use('/', routerIndex)
+
+app.listen(process.env.PORT,function(){
+    console.log(`Running in the port ${process.env.PORT}`)
+})
+
